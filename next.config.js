61bac700cc544a7e84cd4a356f648c9fe4675fff
gabrielvlavianos <64794797+gabrielvlavianos@users.noreply.github.com>
2025-11-25 01:06:02 -0300

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   images: { unoptimized: true },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
