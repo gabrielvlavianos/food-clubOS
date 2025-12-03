28a177f89fa59e74879c0879b60ab15f8cece387
gabrielvlavianos <64794797+gabrielvlavianos@users.noreply.github.com>
2025-12-03 16:58:44 -0300

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
   },
   images: { unoptimized: true },
   poweredByHeader: false,
+  // Generate unique build ID for each deploy
+  generateBuildId: async () => {
+    return `build-${Date.now()}`;
+  },
+  // Disable static page generation cache
+  onDemandEntries: {
+    maxInactiveAge: 0,
+    pagesBufferLength: 0,
+  },
 };
 
 module.exports = nextConfig;
